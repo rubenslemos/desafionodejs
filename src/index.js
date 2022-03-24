7bd2ec2c27c3920b177c51ae8bdae76126ad36be
Rubens <86129671+rubenslemos@users.noreply.github.com>
2022-03-24 11:26:23 -0300

    inicio do desafio
    Iniciando a desenvolver o primeiro desafio do capitulo 1 do curso de nodejs

--- src/index.js
+++ src/index.js
@@ -1,29 +1,52 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers
+  const user = users.find(user => user.username === username)
+  if(!user) {
+    return response.status(400).json({error:" User not found"})
+  }
+  request.user = user
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const { name, username} = request.body
+  users.push({
+    id: uuidv4(),
+    name,
+    username,
+    todos: []
+  })
+  return response.status(201).json({Usuarios: users}).send()
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request
+  return response.json(user.todos)
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { title, deadline } = request.body
+  const { user } = request
+  const todosOperation ={
+    id: uuidv4(),
+    title,
+    done: "false",
+    deadline,
+    created_at: new Date()
+  }
+  user.todos.push(todosOperation)
+  return response.status(201).json([user.todos]).send()
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
